b2f5aed49c3f3e820f55808e43af326b01761b3e
JovinMtl <nsanzumukizath@gmail.com>
2025-10-09 11:46:48 +0200

    asking for google validation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         lang: 'fr'
       },
       meta: [
-        { name: 'google-site-verification', content: 'JB8fK2YOdlu2seSRj8XJ_VuyZ0WsqdlH0d1bHd8rZsE' },
+        // { name: 'google-site-verification', content: 'JB8fK2YOdlu2seSRj8XJ_VuyZ0WsqdlH0d1bHd8rZsE' },
       ]
     }
   }
