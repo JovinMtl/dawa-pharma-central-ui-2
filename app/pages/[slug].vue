629553e1f58b62671e27e39e9dfe9f485d74f037
JovinMtl <nsanzumukizath@gmail.com>
2026-02-10 20:05:10 +0200

    correctly fetching med by GET

---
+++ app/pages/[slug].vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        The slug : {{ slug }} + {{ remoteURL }}
+        <hr>
+        the response: {{ data }}
+    </div>
+</template>
+<script setup lang="ts">
+    import { remoteURL } from '../composables/urlHost'
+
+    // Route
+    const route = useRoute()
+    const slugRaw:string = route.params.slug as string;
+    const slugArray = String(slugRaw).split("=")
+    const slug = slugArray[0];
+    const page = slugArray[1]
+
+
+    const url_post_query = remoteURL + 'api/gOps/search_meds_public/'
+    const { data, status, error, refresh, clear } = useFetch(url_post_query, {
+        params:{
+            'query': slug,
+            'page': page
+        }
+    })
+
+</script>
